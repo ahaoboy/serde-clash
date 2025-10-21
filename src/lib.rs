2a59bff41011602e35f43e2048bad675e97ce178
ahaoboy <504595380@qq.com>
2025-10-21 21:33:23 +0800

    mixed_port

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,17 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Default, Deserialize, Serialize)]
-#[serde(rename_all = "kebab-case")]
+#[serde(default, rename_all = "kebab-case")]
 pub struct Config {
+    #[serde(default)]
     pub port: u16,
+    #[serde(default)]
+    pub mixed_port: u16,
+    #[serde(default)]
     pub socks_port: u16,
     #[serde(default)]
     pub allow_lan: bool,
+    #[serde(default)]
     pub mode: String,
     #[serde(default)]
     #[serde(skip_serializing_if = "Option::is_none")]
@@ -20,13 +25,16 @@ pub struct Config {
     #[serde(default)]
     #[serde(skip_serializing_if = "String::is_empty")]
     pub secret: String,
+    #[serde(default)]
     #[serde(skip_serializing_if = "Option::is_none")]
     pub dns: Option<DnsConfig>,
     #[serde(default)]
     #[serde(skip_serializing_if = "Vec::is_empty")]
     pub proxies: Vec<Proxy>,
+    #[serde(default)]
     #[serde(skip_serializing_if = "Vec::is_empty")]
     pub proxy_groups: Vec<ProxyGroup>,
+    #[serde(default)]
     #[serde(skip_serializing_if = "Vec::is_empty")]
     pub rules: Vec<String>,
 }
